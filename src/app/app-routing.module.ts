88e0452dbecf2bc635042b9675c4d5aecb7d0e30
PabloFranciscoCalandria <pablofcalandria@gmail.com>
2024-06-24 15:52:10 -0300

    Inicio de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'products/:category/:productId',
+    component: ProductDetailComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    //Esto es por si viene cualquier cosa te redireccione en home
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
