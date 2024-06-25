b65eb48f6f7d55cf3ae24b58fd2ea99c569e13a0
PabloFranciscoCalandria <pablofcalandria@gmail.com>
2024-06-25 09:44:07 -0300

    Fin formularios

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
 
     // Formulario de plantilla
 
@@ -23,11 +23,56 @@ export class ContactComponent {
     // Formulario de tipo reactivo
 
     formularioContacto: FormGroup
+    tipoDni: string = 'DNI';
+    
+    // Hacemos de cuenta que lo traemos de otro lado al usuarioActivo
+    // usuarioActivo: any = {
+    //   nombre: 'Juan',
+    //   apellido: 'Perez',
+    //   dni: '25984635'
+    // }
 
     constructor( private form: FormBuilder){
       this.formularioContacto = this.form.group({
-        nombre: ['', Validators.required],
+        nombre: ['', [Validators.required, Validators.minLength(3)]],
+        apellido: [''],
+        tipoDni: [''],
+        dni: [''],
         email: ['', [Validators.required, Validators.email]]
+      });
+    }
+
+    // ngOnInit con validaciones, seteos, disables
+    // ngOnInit(): void {
+      // Podemos hacer los validators aca dentro 
+      // this.formularioContacto.get('apellido')?.setValidators([Validators.required, Validators.minLength(3)]);
+      // this.formularioContacto.get('dni')?.setValidators([Validators.required, Validators.minLength(8)]);
+
+      // Para sacar las validaciones
+      // this.formularioContacto.get('apellido')?.clearValidators();
+      // this.formularioContacto.get('apellido')?.updateValueAndValidity();
+
+      // Esto hace que el valor ya venga seteado y no se pueda modificar
+
+      //Con este get setValue nos deja setear de a 1 solo input
+      // this.formularioContacto.get('nombre')?.setValue(this.usuarioActivo);
+      
+      //Para setear mas de un campo lo hacemos de la siguiente manera
+      // this.formularioContacto.patchValue({
+      //   nombre: this.usuarioActivo.nombre,
+      //   apellido: this.usuarioActivo.apellido,
+      //   dni: this.usuarioActivo.dni
+      // })
+      // this.formularioContacto.get('nombre')?.disable();
+      // this.formularioContacto.get('apellido')?.disable();
+      // this.formularioContacto.get('dni')?.disable();
+    // }
+
+    // ngOnInit con subscripciones
+    ngOnInit(): void {
+      // Escucha todo lo que hace el formulario
+      this.formularioContacto.get('tipoDni')?.valueChanges.subscribe((valor) => {
+        this.tipoDni = valor;
       });
     }
 
